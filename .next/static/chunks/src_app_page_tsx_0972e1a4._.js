447f9a8f87c4403826f6790c6c814d6d91f11da8
hamdan malik <160350536+Humdan150@users.noreply.github.com>
2025-05-19 15:21:17 +0500

    Add .next/static/chunks/src_app_page_tsx_0972e1a4._.js 1747650076219

---
+++ .next/static/chunks/src_app_page_tsx_0972e1a4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7170956a._.js",
+  "static/chunks/src_c18535d4._.js"
+],
+    source: "dynamic"
+});
